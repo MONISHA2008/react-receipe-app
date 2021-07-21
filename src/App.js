bee71c966648a05f890ec86790b4cbbda4d83412
Monisha <monisha.1901116@srec.ac.in>
2021-07-21 22:27:47 +0530

    CONST VALUES

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  return{
+const APP_ID='f29e0388';
+const APP_KEY="aaec13dd7ca17aa20cee721f1b57ec10	—";
+const APP_MOREINFO = "https://swagger.io/.";
+
+  return(
     <div className="App">
-<h1>Hello React</h1>
+<h1> Hello ReactApp </h1>
     </div>
-  };
+
+)  ;
 }
 
 export default App;
